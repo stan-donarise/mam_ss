951185fc21f7a8b7d364cb7f31473fae80c90ccb
stan-donarise <stan-donarise@users.noreply.github.com>
2025-10-01 07:14:14 +0000

    Deploying to gh-pages from @ stan-donarise/mam_ss@cbc9244bfe78b3b01d356ad287705cb5b229c5a2 🚀

--- node.mjs
+++ node.mjs
@@ -6431,6 +6431,9 @@ var $;
 			if(next !== undefined) return next;
 			return null;
 		}
+		activate(next){
+			return (this.event_activate(next));
+		}
 		clicks(next){
 			if(next !== undefined) return next;
 			return null;
@@ -6438,6 +6441,9 @@ var $;
 		event_key_press(next){
 			if(next !== undefined) return next;
 			return null;
+		}
+		key_press(next){
+			return (this.event_key_press(next));
 		}
 		disabled(){
 			return false;
@@ -6468,9 +6474,9 @@ var $;
 		event(){
 			return {
 				...(super.event()), 
-				"click": (next) => (this.event_activate(next)), 
+				"click": (next) => (this.activate(next)), 
 				"dblclick": (next) => (this.clicks(next)), 
-				"keydown": (next) => (this.event_key_press(next))
+				"keydown": (next) => (this.key_press(next))
 			};
 		}
 		attr(){
@@ -6639,7 +6645,7 @@ var $;
             }
             event_key_press(event) {
                 if (event.keyCode === $mol_keyboard_code.enter) {
-                    return this.event_activate(event);
+                    return this.activate(event);
                 }
             }
             tab_index() {
@@ -9884,6 +9890,9 @@ var $;
 		}
 		submit_hint(){
 			return "";
+		}
+		submit_activate(next){
+			return (this.Submit().activate(next));
 		}
 		submit(next){
 			if(next !== undefined) return next;
--- node.test.js
+++ node.test.js
@@ -6422,6 +6422,9 @@ var $;
 			if(next !== undefined) return next;
 			return null;
 		}
+		activate(next){
+			return (this.event_activate(next));
+		}
 		clicks(next){
 			if(next !== undefined) return next;
 			return null;
@@ -6429,6 +6432,9 @@ var $;
 		event_key_press(next){
 			if(next !== undefined) return next;
 			return null;
+		}
+		key_press(next){
+			return (this.event_key_press(next));
 		}
 		disabled(){
 			return false;
@@ -6459,9 +6465,9 @@ var $;
 		event(){
 			return {
 				...(super.event()), 
-				"click": (next) => (this.event_activate(next)), 
+				"click": (next) => (this.activate(next)), 
 				"dblclick": (next) => (this.clicks(next)), 
-				"keydown": (next) => (this.event_key_press(next))
+				"keydown": (next) => (this.key_press(next))
 			};
 		}
 		attr(){
@@ -6630,7 +6636,7 @@ var $;
             }
             event_key_press(event) {
                 if (event.keyCode === $mol_keyboard_code.enter) {
-                    return this.event_activate(event);
+                    return this.activate(event);
                 }
             }
             tab_index() {
@@ -9875,6 +9881,9 @@ var $;
 		}
 		submit_hint(){
 			return "";
+		}
+		submit_activate(next){
+			return (this.Submit().activate(next));
 		}
 		submit(next){
 			if(next !== undefined) return next;
